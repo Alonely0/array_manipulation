6ad6fd94f2b4f9b154878449c8f9f2edb33690dd
guillem <4lon3ly0@tutanota.com>
2022-07-23 01:35:36 +0200

    Optimizations Reduce `memcpy()` calls by using contiguous memory to join arrays

--- README.md
+++ README.md
@@ -2,14 +2,11 @@
 
 This crate exposes 2 traits that allow manipulating arrays in a vec-like fashion.
 
-Alternatives like [ArrayVec](https://docs.rs/arrayvec/latest/arrayvec/struct.ArrayVec.html) operate over a `[MaybeUninit<T>; N]`-like data structure and panic if the size is overflown. 
+Alternatives like [ArrayVec](https://docs.rs/arrayvec/latest/arrayvec/struct.ArrayVec.html) operate over a `[MaybeUninit<T>; N]`-like data structure and panic if the size is overflown. The point of this crate is allowing to "resize" arrays.
 
-Instead, the point of this crate is allowing to "resize" arrays. 
-Internally it just `memcpy()`s stuff around, so a linked list might be preferable as writing a pointer is much faster than copying memory. However, maybe LLVM is able to ultra-optimize this, idk.
+If you heavily depend on this crate, probably a linked list will be a much better fit, but this crate is still very useful for one-time operations, operations where using a linked list will give more problems than solutions, coercing arrays or devices where you can't allocate.
 
-As I said, if you heavily depend on this crate, probably a linked list will be much more performant, but this crate is still very useful for one-time operations, operations where using a linked list will give more problems than solutions, coercing arrays or devices where you can't allocate.
-
-This crate works & performs exceptionally well when you have an array that you need to manipulate but you still need to use a fixed array later and not a Vec. In that situation you just avoided 1 `malloc()` & 1 `memcpy()` best-case scenario and *n* `malloc()`s, 1 `memcpy()` and a conditional worst-case scenario.
+This crate works & performs exceptionally well when you have an array that you need to manipulate but you still need to use a fixed array later and not a Vec. In that situation you just avoided 1 `malloc()` & 3 `memcpy()`s best-case scenario and *n* `malloc()`s, 3 `memcpy()`s and a conditional worst-case scenario.
 
 
 # Merging into core
--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,14 @@
 #![feature(const_refs_to_cell)]
 #![feature(const_transmute_copy)]
 #![feature(const_ptr_read)]
+#![feature(const_slice_index)]
 #![feature(specialization)]
 #![cfg_attr(not(test), no_std)]
 #![doc = include_str!("../README.md")]
 
 use core::{
-    mem::{forget, transmute_copy, MaybeUninit},
-    ptr::{copy_nonoverlapping, drop_in_place, read},
+    mem::{forget, ManuallyDrop},
+    ptr::{drop_in_place, read},
 };
 
 /// Holds the append methods.
@@ -23,7 +24,6 @@ use core::{
 // TODO implement append_at & concat_at when const exprs become usable enough
 pub trait ArrayAdd<T, const N: usize>: Sized {
     /// Inserts an element at the end of Self. Use concat for >1 elements.
-    /// In order to avoid unnecessary calls to `memcpy()`.
     /// # Examples
     /// ```
     /// use array_manipulation::ArrayAdd;
@@ -36,7 +36,6 @@ pub trait ArrayAdd<T, const N: usize>: Sized {
     fn append(self, e: T) -> [T; N + 1];
 
     /// Inserts an element at the start of Self. Use concat for >1 elements.
-    /// In order to avoid unnecessary calls to `memcpy()`.
     /// # Examples
     /// ```
     /// use array_manipulation::ArrayAdd;
@@ -49,8 +48,6 @@ pub trait ArrayAdd<T, const N: usize>: Sized {
     fn append_back(self, e: T) -> [T; N + 1];
 
     /// Takes an array of L elements and appends it at the end of Self.
-    /// Performs 2 calls to `memcpy()`, so if your code heavily uses it
-    /// maybe a linked list is a better fit for your use case.
     /// # Examples
     /// ```
     /// use array_manipulation::ArrayAdd;
@@ -63,8 +60,6 @@ pub trait ArrayAdd<T, const N: usize>: Sized {
     fn concat<const L: usize>(self, array: [T; L]) -> [T; N + L];
 
     /// Takes an array of L elements and appends it at the end of Self.
-    /// Performs 2 calls to `memcpy()`, so if your code heavily uses it
-    /// maybe a linked list is a better fit for your use case.
     /// # Examples
     /// ```
     /// use array_manipulation::ArrayAdd;
@@ -85,7 +80,7 @@ pub trait ArrayAdd<T, const N: usize>: Sized {
 pub trait ArrayRemove<T, const N: usize>: Sized {
     /// `memcpy()`s all the elements on an array except the first L ones.
     /// Basically it creates a new fixed-size array with all the
-    /// elements except the first one. Won't compile if N == 0.
+    /// elements except the first L ones. Won't compile if N == 0.
     /// # Examples
     /// ```
     /// use array_manipulation::ArrayManipulation;
@@ -97,9 +92,9 @@ pub trait ArrayRemove<T, const N: usize>: Sized {
     /// ```
     fn truncate_start<const L: usize>(self) -> [T; N - L];
 
-    /// `memcpy()`s all the elements on an array except the last L ones.
-    /// Basically it creates a new fixed-size array with all the
-    /// elements except the first one. Won't compile if N == 0.
+    /// Creates a new fixed-size array with all the
+    /// elements except the L ones.
+    /// Won't compile if N == 0.
     /// # Examples
     /// ```
     /// use array_manipulation::ArrayManipulation;
@@ -112,114 +107,50 @@ pub trait ArrayRemove<T, const N: usize>: Sized {
     fn truncate_end<const L: usize>(self) -> [T; N - L];
 }
 
-impl<T, const N: usize> const ArrayAdd<T, N> for [T; N] {
-    default fn concat<const L: usize>(self, array: [T; L]) -> [T; N + L] {
-        let mut result: MaybeUninit<[T; N + L]> = MaybeUninit::uninit();
-        unsafe {
-            copy_nonoverlapping(&raw const self, result.as_mut_ptr().cast(), 1); // copy elements
-            copy_nonoverlapping(
-                &raw const array,
-                result.as_mut_ptr().cast::<T>().add(N).cast(),
-                1,
-            ); // copy elements
+#[repr(C)]
+struct Contiguous<A, B>(A, B);
 
-            // avoid drop & deallocation of the copied elements
-            forget(self);
-            forget(array);
-
-            result.assume_init() // initialized
-        }
+// from https://github.com/Vurich/const-concat/issues/13#issue-1190857331
+const unsafe fn transmute_unchecked<From, To>(from: From) -> To {
+    union Transmute<From, To> {
+        from: ManuallyDrop<From>,
+        to: ManuallyDrop<To>,
     }
 
-    default fn concat_back<const L: usize>(self, array: [T; L]) -> [T; N + L] {
-        let mut result: MaybeUninit<[T; N + L]> = MaybeUninit::uninit();
-        unsafe {
-            copy_nonoverlapping(&raw const array, result.as_mut_ptr().cast(), 1); // copy elements
-            copy_nonoverlapping(
-                &raw const self,
-                result.as_mut_ptr().cast::<T>().add(L).cast(),
-                1,
-            ); // copy elements
-
-            // avoid drop & deallocation of the copied elements
-            forget(self);
-            forget(array);
-
-            result.assume_init() // initialized
+    ManuallyDrop::into_inner(
+        Transmute {
+            from: ManuallyDrop::new(from),
         }
-    }
-
-    default fn append(self, e: T) -> [T; N + 1] {
-        let mut result: MaybeUninit<[T; N + 1]> = MaybeUninit::uninit();
-        unsafe {
-            copy_nonoverlapping(&raw const self, result.as_mut_ptr().cast(), 1); // copy elements
-            copy_nonoverlapping(&raw const e, result.as_mut_ptr().cast::<T>().add(N), 1); // copy element
-
-            // avoid drop & deallocation of the copied elements
-            forget(self);
-            forget(e);
-
-            result.assume_init()
-        }
-    }
-
-    default fn append_back(self, e: T) -> [T; N + 1] {
-        let mut result: MaybeUninit<[T; N + 1]> = MaybeUninit::uninit();
-        unsafe {
-            copy_nonoverlapping(&raw const e, result.as_mut_ptr().cast::<T>(), 1); // copy element
-            copy_nonoverlapping(
-                &raw const self,
-                result.as_mut_ptr().cast::<T>().add(1).cast(),
-                1,
-            ); // copy elements
-
-            // avoid drop & deallocation of the copied elements
-            forget(self);
-            forget(e);
-
-            result.assume_init()
-        }
-    }
+        .to,
+    )
 }
 
-impl<T: Copy, const N: usize> const ArrayAdd<T, N> for [T; N] {
+impl<T, const N: usize> const ArrayAdd<T, N> for [T; N] {
     fn concat<const L: usize>(self, array: [T; L]) -> [T; N + L] {
-        let mut result: MaybeUninit<[T; N + L]> = MaybeUninit::uninit();
         unsafe {
-            *result.as_mut_ptr().cast() = self; // write
-            *result.as_mut_ptr().cast::<T>().add(N).cast() = array; // offset ptr & write
-            result.assume_init() // initialized
+            // join contiguous memory in a single array
+            transmute_unchecked(Contiguous(self, array))
         }
     }
 
     fn concat_back<const L: usize>(self, array: [T; L]) -> [T; N + L] {
-        let mut result: MaybeUninit<[T; N + L]> = MaybeUninit::uninit();
         unsafe {
-            *result.as_mut_ptr().cast() = array; // write
-            *result.as_mut_ptr().cast::<T>().add(L).cast() = self; // offset ptr & write
-            result.assume_init() // initialized
+            // join contiguous memory in a single array
+            transmute_unchecked(Contiguous(array, self))
         }
     }
 
-    fn append(self, e: T) -> [T; N + 1] {
-        let mut result: MaybeUninit<[T; N + 1]> = MaybeUninit::uninit();
+    fn append(self, element: T) -> [T; N + 1] {
         unsafe {
-            copy_nonoverlapping(&raw const self, result.as_mut_ptr().cast(), 1); // copy elements
-            *result.as_mut_ptr().cast::<T>().add(N) = e; // offset ptr & write
-            result.assume_init()
+            // join contiguous memory in a single array
+            transmute_unchecked(Contiguous(self, element))
         }
     }
 
-    fn append_back(self, e: T) -> [T; N + 1] {
-        let mut result: MaybeUninit<[T; N + 1]> = MaybeUninit::uninit();
+    fn append_back(self, element: T) -> [T; N + 1] {
         unsafe {
-            *result.as_mut_ptr().cast::<T>() = e; // offset ptr & write
-            copy_nonoverlapping(
-                &raw const self,
-                result.as_mut_ptr().cast::<T>().add(1).cast(),
-                1,
-            ); // copy elements
-            result.assume_init()
+            // join contiguous memory in a single array
+            transmute_unchecked(Contiguous(element, self))
         }
     }
 }
@@ -235,9 +166,7 @@ impl<T, const N: usize> const ArrayRemove<T, N> for [T; N] {
 
     default fn truncate_end<const L: usize>(self) -> [T; N - L] {
         unsafe {
-            let result = read((&raw const self).cast()); // copy from offset'ed pointer
-            forget(self); // avoid drop & deallocation of the copied elements
-            result
+            transmute_unchecked(self) // resize self
         }
     }
 }
@@ -246,18 +175,16 @@ impl<T, const N: usize> const ArrayRemove<T, N> for [T; N] {
 impl<T: Drop, const N: usize> ArrayRemove<T, N> for [T; N] {
     fn truncate_start<const L: usize>(mut self) -> [T; N - L] {
         unsafe {
-            drop_in_place(&raw mut self[..L]); // drop popped element
             let result = read((&raw const self).cast::<T>().add(L).cast()); // copy from offset'ed pointer
+            drop_in_place(&raw mut self[..L]); // drop popped elements
             forget(self); // avoid drop & deallocation of the copied elements
             result
         }
     }
     fn truncate_end<const L: usize>(mut self) -> [T; N - L] {
         unsafe {
-            drop_in_place(&raw mut self[L..]); // drop popped element
-            let result = transmute_copy(&self); // copy elements
-            forget(self); // avoid drop & deallocation of the copied elements
-            result
+            drop_in_place(&raw mut self[L..]); // drop popped elements
+            transmute_unchecked(self) // resize self
         }
     }
 }
@@ -273,7 +200,7 @@ mod tests {
         let result = input.append(vec![5, 6, 7]);
         assert_eq!(expected, result)
     }
-    
+
     #[test]
     fn append_back_noncopy() {
         let input = [vec![1, 2], vec![3, 4]];
@@ -281,23 +208,35 @@ mod tests {
         let result = input.append_back(vec![254, 255, 0]);
         assert_eq!(expected, result)
     }
-    
+
     #[test]
     fn concat_noncopy() {
         let input = [vec![1, 2], vec![3, 4]];
-        let expected = [vec![1, 2], vec![3, 4], vec![5, 6, 7], vec![8, 9], vec![10, 11, 12]];
+        let expected = [
+            vec![1, 2],
+            vec![3, 4],
+            vec![5, 6, 7],
+            vec![8, 9],
+            vec![10, 11, 12],
+        ];
         let result = input.concat([vec![5, 6, 7], vec![8, 9], vec![10, 11, 12]]);
         assert_eq!(expected, result)
     }
-    
+
     #[test]
     fn concat_back_noncopy() {
         let input = [vec![1, 2], vec![3, 4]];
-        let expected = [vec![249, 250, 251], vec![252, 253], vec![254, 255, 0], vec![1, 2], vec![3, 4]];
+        let expected = [
+            vec![249, 250, 251],
+            vec![252, 253],
+            vec![254, 255, 0],
+            vec![1, 2],
+            vec![3, 4],
+        ];
         let result = input.concat_back([vec![249, 250, 251], vec![252, 253], vec![254, 255, 0]]);
         assert_eq!(expected, result)
     }
-    
+
     #[test]
     fn truncate_start_noncopy() {
         let input = [vec![1, 2], vec![3, 4], vec![5, 6], vec![7, 8]];
@@ -305,7 +244,7 @@ mod tests {
         let result = input.truncate_start::<2>();
         assert_eq!(expected, result)
     }
-    
+
     #[test]
     fn truncate_end_noncopy() {
         let input = [vec![1, 2], vec![3, 4], vec![5, 6], vec![7, 8]];
@@ -321,7 +260,7 @@ mod tests {
         let result = input.append(5);
         assert_eq!(expected, result)
     }
-    
+
     #[test]
     fn append_back_copy() {
         let input = [1, 2, 3, 4];
@@ -329,7 +268,7 @@ mod tests {
         let result = input.append_back(0);
         assert_eq!(expected, result)
     }
-    
+
     #[test]
     fn concat_copy() {
         let input = [1, 2, 3, 4];
@@ -337,7 +276,7 @@ mod tests {
         let result = input.concat([5, 6, 7]);
         assert_eq!(expected, result)
     }
-    
+
     #[test]
     fn concat_back_copy() {
         let input = [1, 2, 3, 4];
@@ -345,7 +284,7 @@ mod tests {
         let result = input.concat_back([254, 255, 0]);
         assert_eq!(expected, result)
     }
-    
+
     #[test]
     fn truncate_start_copy() {
         let input = [1, 2, 3, 4];
@@ -353,7 +292,7 @@ mod tests {
         let result = input.truncate_start::<2>();
         assert_eq!(expected, result)
     }
-    
+
     #[test]
     fn truncate_end_copy() {
         let input = [1, 2, 3, 4];
